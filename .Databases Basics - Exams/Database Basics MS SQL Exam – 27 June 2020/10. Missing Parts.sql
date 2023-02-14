3433679b86fa0fd9054d1847c9030c445e34fd21
Kalsito1017 <kurabiika34@abv.bg>
2023-02-14 21:57:24 +0200

    Added Exercise

--- .Databases Basics - Exams/Database Basics MS SQL Exam – 27 June 2020/10. Missing Parts.sql
+++ .Databases Basics - Exams/Database Basics MS SQL Exam – 27 June 2020/10. Missing Parts.sql
@@ -1,9 +1,23 @@
-SELECT * FROM Parts p
-JOIN PartsNeeded pn ON pn.PartId = p.PartId
-JOIN OrderParts op ON op.PartId = p.PartId
-JOIN Jobs j ON j.JobId = pn.JobId
-LEFT JOIN Orders AS o ON o.JobId = j.JobId
-WHERE j.Status != 'Finished' AND p.StockQty + op.Quantity < pn.Quantity
-
+SELECT 
+	p.PartId,
+	p.Description,
+	SUM(pn.Quantity) AS [Required],
+	SUM(p.StockQty) AS [In Stock],
+	ISNULL(SUM(z.Quantity), 0) AS Ordered
+FROM Parts AS p
+LEFT JOIN PartsNeeded AS pn ON pn.PartId = p.PartId
+LEFT JOIN Jobs AS j ON j.JobId = pn.JobId
+LEFT JOIN (
+		SELECT 
+			op.PartId,
+			op.Quantity
+		FROM Orders AS o
+		JOIN OrderParts AS op ON op.OrderId = o.OrderId
+		WHERE o.Delivered = 0
+		) AS z ON z.PartId = p.PartId
+	WHERE j.Status <> 'Finished'
+GROUP BY p.PartId, p.Description
+HAVING SUM(pn.Quantity) > SUM(p.StockQty) + ISNULL(SUM(z.Quantity), 0)
+ORDER BY p.PartId
 
 
